971f3e58029976b3e14ce6fa5f1fe82ac6b93f7b
Ata Hakkak <ata.hakkak@gmail.com>
2024-04-01 15:26:16 +0200

    added selected parking rendering

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import LoginPage from "./LandingPage/LoginPage";
 import SignupPage from "./LandingPage/SignupPage";
 import AuthRouter from "./LandingPage/AuthRouter"; // Import the AuthRouter component
 import MapPage from "./components/MapPage";
-import FullParkingDetails from "./components/FullParkingDetails"; /* delete later */
 
 const App = () => {
   return (
@@ -18,8 +17,7 @@ const App = () => {
             <Route index element={<ExplanationContainer />} />
           </Route>
           <Route path="map" element={<MapPage />} />
-          <Route path="details/:id" element={<FullParkingDetails />} />
-          {/* delete later */}
+          <Route path="/map/details/:id" element={<MapPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/auth" element={<AuthRouter />} />
--- src/components/MapPage.jsx
+++ src/components/MapPage.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router";
 import parkings from "../api/parkings.json";
+import { Link } from "react-router-dom";
 
 export default function MapPage() {
   /* const API = "http://localhost:8081/suggestedParkings"; 
@@ -19,28 +21,54 @@ export default function MapPage() {
 
     fetchData();
   }, []); */
+  const { id } = useParams();
+  const [selectedParking, setSelectedParking] = useState(null);
+
+  useEffect(() => {
+    if (id) {
+      const parking = parkings.find((parking) => parking.id === parseInt(id));
+      setSelectedParking(parking);
+    } else {
+      setSelectedParking(null);
+    }
+  }, [id]);
 
   return (
-    <div className="mapPageContainer m-2 flex gap-2">
-      <div className="suggestionCardsContainer w-1/4 ">
-        {parkings.map((parking) => {
-          return (
-            <div
-              className="border rounded shadow min-h-24 pb-2 mb-2 "
-              key={parking._id}
-            >
-              <h3 className="font-medium px-2 text-center  bg-red-600">
-                Street name(from form)
-              </h3>
-              <h3 className="px-2 text-center mt-5">
-                Hourly Price: ${parking.location.hourlyPrice}
-              </h3>
-              <a href=""></a>
-            </div>
-          );
-        })}
-      </div>
-      <div className="map bg-green-200 w-3/4 mb-2">
+    <div className="mapPageContainer m-2 flex gap-2 min-h-screen">
+      {selectedParking ? (
+        <div className="suggestionCardsContainer w-1/4 ">
+          <div
+            className="border rounded shadow min-h-24 pb-2 mb-2 "
+            key={selectedParking.id}
+          >
+            <h3 className="font-medium px-2 text-center  bg-red-600">
+              Street name(from form)
+            </h3>
+            <h3 className="px-2 text-center mt-5">
+              Hourly Price: ${selectedParking.location.hourlyPrice}
+            </h3>
+          </div>
+        </div>
+      ) : (
+        <div className="suggestionCardsContainer w-1/4 min-h-screen">
+          {parkings.map((parking) => (
+            <Link to={`details/${parking.id}`} key={parking.id}>
+              <div
+                className="border rounded shadow min-h-24 pb-2 mb-2 "
+                key={parking.id}
+              >
+                <h3 className="font-medium px-2 text-center  bg-red-600">
+                  Street name(from form)
+                </h3>
+                <h3 className="px-2 text-center mt-5">
+                  Hourly Price: ${parking.location.hourlyPrice}
+                </h3>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
+      <div className="map bg-green-200 w-3/4 mb-2 min-h-screen">
         <h1 className="font-medium text-center">Map</h1>
       </div>
     </div>
