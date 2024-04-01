9fcd9bd93bef0936c0d748207f3cdd426df93dce
Ata Hakkak <ata.hakkak@gmail.com>
2024-04-02 01:15:27 +0200

    tailwind forms

--- src/LandingPage/LoginPage.jsx
+++ src/LandingPage/LoginPage.jsx
@@ -10,29 +10,61 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="auth-container">
-      <div className="auth-form">
-        <h2>Login</h2>
-        <form onSubmit={handleLoginSubmit}>
+    <div className="logincontainer flex items-center">
+      <form
+        onSubmit={handleLoginSubmit}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 ml-[34%] w-96"
+      >
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="Email"
+          >
+            Email
+          </label>
           <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
             type="email"
             placeholder="Email"
+            required="true"
             value={loginData.email}
             onChange={(e) =>
               setLoginData({ ...loginData, email: e.target.value })
             }
           />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+            required="true"
+          >
+            Password
+          </label>
           <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
             type="password"
-            placeholder="Password"
+            placeholder="**********"
             value={loginData.password}
             onChange={(e) =>
               setLoginData({ ...loginData, password: e.target.value })
             }
           />
-          <button type="submit">Login</button>
-        </form>
-      </div>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="px-6 py-2 text-lg font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+            type="submit"
+          >
+            Login
+          </button>
+        </div>
+      </form>
+      {/* <p className="text-center text-gray-500 text-xs">
+        &copy;2024 Private Parking All rights reserved.
+      </p> */}
     </div>
   );
 };
