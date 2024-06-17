d73f066fd49aebaea259d3cee085d9ecc2c64700
Ata Hakkak <ata.hakkak@gmail.com>
2024-06-17 16:55:38 +0200

    team adjustments

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -1,6 +1,3 @@
-import IsakImg from "../assets/Jaguar F TYPE Luxury Sports Blue Car - 1280x559.png";
-import SaeedImg from "../assets/Red Ferrari 458 Italia Sports Car - 1205x770.png";
-import AtaImg from "../assets/Infiniti Essence Concept Sports Car - 1280x586.png";
 import styles from "./Career.module.css";
 
 function Career() {
@@ -10,20 +7,7 @@ function Career() {
       <h3></h3>
 
       <div className={styles.CareerContainer}>
-        <div className={styles.Isak}>
-          <img src={IsakImg} alt="Isak image" className={styles.SaeedImg} />
-          <h2>Mr. Isak</h2>
-          <p>
-            As a developer, you'll contribute to creating seamless solutions
-            that help users easily find parking spots, enhancing their overall
-            enjoyment of the process. Join our dynamic team of developers and be
-            at the forefront of crafting innovative solutions that revolutionize
-            the way people experience urban mobility.
-          </p>
-        </div>
-
         <div className={styles.Saeed}>
-          <img src={SaeedImg} alt="Saeed image" className={styles.IsakImg} />
           <h2>Mr. Saeed</h2>
           <p>
             As part of a creative team, I would say there is enough room to
@@ -35,7 +19,6 @@ function Career() {
         </div>
 
         <div className={styles.Ata}>
-          <img src={AtaImg} alt="Ata image" className={styles.AtaImg} />
           <h2>Mr. Ata</h2>
           <p>
             Join our dynamic team and explore endless opportunities for personal
